3bc34c97e7ddb4fa3395bf6b6b8b97045bb0bdf7
Kim Jun Hyeong <kheami35@gmail.com>
2025-08-25 16:26:39 +0900

    [ Init ] Player.cs

--- Inventory/Assets/Scripts/Uis/MainUI.cs
+++ Inventory/Assets/Scripts/Uis/MainUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestUI : UIBase
+public class MainUI : UIBase
 {
     // Start is called before the first frame update
     void Start()
